3adf284e5836abc33f6475091680f09f564072d7
manuel <mzegarra@csonic.cloud>
2023-06-03 23:43:04 -0500

    add config

--- src/Api/POC.MSPago.WebApi/Controllers/WeatherForecastController.cs
+++ src/Api/POC.MSPago.WebApi/Controllers/WeatherForecastController.cs
@@ -6,27 +6,21 @@ namespace POC.MSPago.WebApi.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+    private readonly IHostEnvironment hostEnvironment;
+
 
     private readonly ILogger<WeatherForecastController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IHostEnvironment hostEnvironment)
     {
         _logger = logger;
+        this.hostEnvironment = hostEnvironment;
+
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public string Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return $"Development: {hostEnvironment.IsDevelopment} - Production: {hostEnvironment.IsProduction}" ;
     }
 }
