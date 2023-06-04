2d604af76b7a8a68fead1e37f9922284f16248d1
manuel <mzegarra@csonic.cloud>
2023-06-04 01:00:37 -0500

    add config

---
+++ src/Core/POC.MSPago.Application/Features/Payments/Config/MicroserviceProperties.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace POC.MSPago.Application.Features.Payments.Config;
+
+public class MicroserviceProperties
+{
+
+
+    [ConfigurationKeyName("Izipay")]
+    public IzipayProperties IzipayProperties { get; set; }
+
+    [ConfigurationKeyName("Alignet")]
+    public AlignetProperties AlignetProperties { get; set; }
+}
---
+++ src/Core/POC.MSPago.Application/Features/Payments/Config/StoreProperties.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace POC.MSPago.Application.Features.Payments.Config;
+
+public class StoreProperties
+{
+    public string Url { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Currency { get; set; }
+}
+
+public class IzipayProperties
+{
+    [ConfigurationKeyName("Stores")]
+    public Dictionary<string, StoreProperties> Stores { get; set; }
+
+    /*public IzipayProperties() {
+        this.Stores = new Dictionary<string, StoreProperties>();
+    }*/
+}
